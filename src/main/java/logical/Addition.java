82c28756fa63f7247358d31fae158b320ad79b43
Vishal Dhome <vishaldhome@192.168.0.104>
2023-06-25 16:32:52 +0530

    First Commit

---
+++ src/main/java/logical/Addition.java
@@ -0,0 +1,21 @@
+package logical;
+
+public class Addition {
+	
+	public int addition(int a, int b) {
+		
+		int c=a+b;
+		
+		return c;	
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		Addition a = new Addition();
+		a.addition(10, 20);
+		
+		
+	}
+
+}
